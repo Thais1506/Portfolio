90f4d6a2d9adbaade1bf5f40d209c907a31e33c0
Thais1506 <thaispascoalsje@gmail.com>
2023-04-30 13:42:06 -0300

    portfolio

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <header>
+        <h2>PORTFOLIO</h2>
+      </header>
+
+      <div className='img-container'>
+        <img className='imagem' src="https://www.hypeness.com.br/1/2019/10/Ce%CC%81u_Roxo_Tufa%CC%83o_6.jpg" alt="thaisinha mo de vida" />
+      </div>
+      
+    </div>
+  );
+}
+
+export default App
